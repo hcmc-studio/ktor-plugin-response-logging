1a6a300134f8e4e84bf65d9f8d1245225b5e28d1
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-15 15:26:52 +0900

    [Script push]

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+val project_version: String by project
+val jdk_version: String by project
+val ktor_version: String by project
+
+plugins {
+    kotlin("jvm")
+    id("maven-publish")
+}
+
+group = "studio.hcmc"
+version = project_version
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(jdk_version.toInt())
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "studio.hcmc"
+            artifactId = project.name
+            version = project_version
+            from(components["java"])
+        }
+    }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+}
---
+++ src/main/kotlin/studio/hcmc/ktor/plugin/ResponseLogging.kt
@@ -0,0 +1,25 @@
+package studio.hcmc.ktor.plugin
+
+import io.ktor.server.application.*
+import org.slf4j.LoggerFactory
+import org.slf4j.event.Level
+
+class ResponseLoggingConfiguration {
+    var level = Level.TRACE
+    var logger = LoggerFactory.getLogger("ResponseLogging")
+    var format: suspend (call: ApplicationCall, body: Any) -> String = { _, _ -> throw NotImplementedError() }
+}
+
+val ResponseLogging = createApplicationPlugin("ResponseLogging", ::ResponseLoggingConfiguration) {
+    val print: (msg: String) -> Unit = when (pluginConfig.level) {
+        Level.ERROR -> pluginConfig.logger::error
+        Level.WARN -> pluginConfig.logger::warn
+        Level.INFO -> pluginConfig.logger::info
+        Level.DEBUG -> pluginConfig.logger::debug
+        Level.TRACE -> pluginConfig.logger::trace
+    }
+
+    onCallRespond { call, body ->
+        print(pluginConfig.format(call, body))
+    }
+}
